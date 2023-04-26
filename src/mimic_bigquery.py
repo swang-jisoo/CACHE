e593d3031a76757f722c46105c1231fea073c4f3
jisoolee <swang.jislee@gmail.com>
2023-04-26 11:29:32 +0000

    Install gcloud CLI and libraries for bigquery

---
+++ src/mimic_bigquery.py
@@ -0,0 +1,34 @@
+# Import libraries
+import os
+import pandas as pd
+
+# Import for accessing MIMIC dataset using Google BigQuery.
+from google.cloud import bigquery
+
+# Run the codes below on terminal
+# gcloud auth login
+# gcloud auth application-default login
+# gcloud iam service-accounts create swang.jislee@gmail.com
+# gcloud config set project clinical-entity-extraction
+
+project_id = 'clinical-entity-extraction'
+os.environ["GOOGLE_CLOUD_PROJECT"] = project_id
+
+# Read data from BigQuery into pandas dataframes.
+def run_query(query, project_id=project_id):
+  return pd.io.gbq.read_gbq(
+      query,
+      project_id=project_id,
+      dialect='standard')
+
+dataset = 'mimiciii_demo'
+subject_id = 10017
+
+query = f"""
+SELECT *
+FROM `physionet-data.{dataset}.patients`
+WHERE subject_id = {subject_id}
+"""
+pt = run_query(query)
+# pt['anchor_year'] = pt['anchor_year']
+pt
