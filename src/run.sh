ff52203eb3ded584488fd0089ea3e8042433a612
jisoolee <swang.jislee@gmail.com>
2023-05-15 12:11:18 +0000

    edit run.sh to run preprocessed MIMIC

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-python train.py --dname=mimic3 --epochs=100 --cuda=1 --num_labels=25 --num_nodes=100 --num_labeled_data=500
+python train.py --dname=mimic3 --epochs=100 --cuda=1 --num_labels=25 --num_nodes=7423 --num_labeled_data=12353 --feature_dim=128 --Classifier_hidden=128
+#python train.py --dname=mimic3 --epochs=100 --cuda=1 --num_labels=25 --num_nodes=100 --num_labeled_data=500
 #python train.py --dname=cradle --epochs=100 --cuda=1 --num_labels=1 --num_nodes=200 --num_labeled_data=all
 
 #python train.py --dname=mimic3 --epochs=100 --cuda=1 --num_labels=25 --num_nodes=100 --num_labeled_data=500 --vanilla
